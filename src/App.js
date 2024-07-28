a894dad22589b69ad86d3c2e8e5d94eade5163eb
kunika18 <129682280+kunika18@users.noreply.github.com>
2024-07-28 21:34:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './Components/login';
-import { Signup } from './Components/signup';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        
       </Routes>
     </Router>
   );
