90a7cf1baccf9b907b38fce800ec0f94d8d02cef
Lavanya Pathak <114918785+LavanyaPathak17@users.noreply.github.com>
2024-07-28 15:00:43 +0530

    Update App.js and added routes for Signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './Components/login';
+import {Signup }  from './Components/signup';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        
+         <Route path="/signup" element = {<Signup />} />
       </Routes>
     </Router>
   );
