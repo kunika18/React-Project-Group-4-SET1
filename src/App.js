b0c33ba071078362760b99ab751457942d1df51a
kunika18 <129682280+kunika18@users.noreply.github.com>
2024-07-28 21:19:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from './Components/login';
+import { Signup } from './Components/signup';
 
 function App() {
   return (
