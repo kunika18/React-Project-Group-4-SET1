e8468fc4795b81fc2ee16106e07c83aa81a898f3
Lavanya Pathak <114918785+LavanyaPathak17@users.noreply.github.com>
2024-07-30 14:39:46 +0530

    undo changes in logout button

--- src/Components/Tab/Navbar.js
+++ src/Components/Tab/Navbar.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import { FaBell } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavBar.css'; // Import custom CSS
 
 const NavBar = ({ userName, onLogout }) => {
-  const navigate = useNavigate();
-  const handleLogout = () => {
-      navigate('/');
-  };
-
   return (
     <Navbar expand="lg" className="custom-navbar">
       <Container fluid className="nav-container">
         <Nav className="welcome-nav mx-auto">
           <Navbar.Brand href="#" className="welcome-message">
-            WELCOME User {username} !!
+            WELCOME User
           </Navbar.Brand>
         </Nav>
         <Nav className="ml-auto d-flex align-items">
           <Nav.Link href="#" className="bell-icon">
             <FaBell />
           </Nav.Link>
-          <Button variant="danger" onclick ={handleLogout} className="logout-button">
+          <Button variant="danger" onClick={onLogout} className="logout-button">
             Logout
           </Button>
         </Nav>
@@ -33,3 +27,4 @@ const NavBar = ({ userName, onLogout }) => {
 };
 
 export default NavBar;
+
